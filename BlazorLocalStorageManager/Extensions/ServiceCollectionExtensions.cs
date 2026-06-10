35c8c2f1de165520be77b8c1f307175752a92fbd
Siavash Nabipour <siavashnabipour@gmail.com>
2026-06-10 18:50:50 +0330

    Add project files.

---
+++ BlazorLocalStorageManager/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BlazorLocalStorageManager.Interfaces;
+using BlazorLocalStorageManager.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorLocalStorageManager.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBrowserStorage(this IServiceCollection services)
+        {
+            services.AddScoped<ILocalStorageService, LocalStorageService>();
+
+            return services;
+        }
+    }
+}
---
+++ BlazorLocalStorageManager/Interfaces/ILocalStorageService.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorLocalStorageManager.Interfaces
+{
+    public interface ILocalStorageService
+    {
+        ValueTask SetAsync<T>(string key, T value);
+        ValueTask<T?> GetAsync<T>(string key);
+        ValueTask RemoveAsync(string key);
+        ValueTask ClearAsync();
+    }
+}
